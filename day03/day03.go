e4339e035e292c15cb2d94960054aae1312bbb88
Christian Decker <decker_christian@bah.com>
2024-12-04 00:46:52 -0500

    Setup day 3

---
+++ day03/day03.go
@@ -0,0 +1,19 @@
+package day03
+
+import "fmt"
+
+func Part1() int {
+	return 0
+}
+
+func Part2() int {
+	return 0
+}
+
+func Solve() {
+	answer1 := Part1()
+	fmt.Printf("Answer: %v\n\n", answer1)
+
+	answer2 := Part2()
+	fmt.Printf("Answer: %v\n\n", answer2)
+}
